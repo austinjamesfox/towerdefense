909b869873a1a0c4adf503614f7cc8978ba575a8
Austin <austinjamesfox1@gmail.com>
2026-08-16 20:04:21 -0400

    Started working on round/wave spawning

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -4,7 +4,27 @@ extends Node2D
 
 @export var enemy_scene : PackedScene
 
+@export var enemyOne : PackedScene
+
 @export var gameManager : Node
+
+var rounds = {
+	#Round: ["Enemy Order", "Quantity per Enemy Order", Spawn Rate]
+	#"One": [10, 10],
+	#"Two": ["1111",0.5]
+	1: [
+		{
+			"enemy": enemyOne,
+			"quantity": 10,
+			"spawn_rate": 1.0
+		},
+		{
+			"enemy": enemyOne,
+			"quantity": 5,
+			"spawn_rate": 0.5
+		}
+		]
+}
 
 func _on_timer_timeout() -> void:
 	# Spawn Enemy
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,14 +1,22 @@
 extends Node
 
 signal player_health_modified(healthValue : int)
+signal round_lost
 
 var playerHealth : int = 100:
 	set(value):
 		playerHealth = clampi(value, 0, 500)
 		player_health_modified.emit(playerHealth)
+		if playerHealth == 0:
+			game_over()
 
 	get:
 		return playerHealth
 
 func player_lose_health(healthLost : int):
 	playerHealth -= healthLost
+
+func game_over():
+	round_lost.emit()
+	print("Game Over!")
+	Engine.time_scale = 0
