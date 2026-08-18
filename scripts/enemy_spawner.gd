7547e1e5eb05798be18fb9a71a98f96b549748ea
Austin <austinjamesfox1@gmail.com>
2026-08-17 21:38:22 -0400

    Still working on wave spawning

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -2,16 +2,15 @@ extends Node2D
 
 @export var enemyPath : Path2D
 
-@export var enemy_scene : PackedScene
+@export var enemy : PackedScene
 
 @export var enemyOne : PackedScene
 
 @export var gameManager : Node
 
-var rounds = {
-	#Round: ["Enemy Order", "Quantity per Enemy Order", Spawn Rate]
-	#"One": [10, 10],
-	#"Two": ["1111",0.5]
+var readyToSpawn : bool = false
+
+@onready var rounds = {
 	1: [
 		{
 			"enemy": enemyOne,
@@ -28,6 +27,27 @@ var rounds = {
 
 func _on_timer_timeout() -> void:
 	# Spawn Enemy
-	var enemy = enemy_scene.instantiate()
-	enemyPath.add_child(enemy)
+	round_spawn()
+	#var enemy = enemy_scene.instantiate()
+	#enemyPath.add_child(enemy)
 	#gameManager.player_lose_health(10)
+
+# The current round variable should be managed in the GameManager instead. This is only temp.
+@export var currentRound = 1
+
+func round_spawn():
+	for spawnGroup in rounds[currentRound]:
+		for i in range(spawnGroup["quantity"]):
+			var enemy = spawnGroup["enemy"].instantiate()
+			enemyPath.add_child(enemy)
+			createTimer(spawnGroup["spawn_rate"])
+			
+			
+		
+func createTimer(spawnRate):
+	var spawnTimer = Timer.new()
+	spawnTimer.wait_time = spawnRate
+	spawnTimer.one_shot = true
+	add_child(spawnTimer)
+	spawnTimer.start()
+	
